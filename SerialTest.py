da5bd9b257e604ca83a92fab816d8df4f8ab421e
Jonathan Morris <catslover32@gmail.com>
2021-11-18 19:45:47 -0500

    Added yet another file

--- SerialTest.py
+++ SerialTest.py
@@ -1,14 +1,29 @@
 import serial
+import paho.mqtt.client as paho
+import struct
 
-ser = serial.Serial('COM9',11520)
+broker="127.0.0.1"
+port=1883
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+
+client1= paho.Client("control1")                           #create client object
+client1.on_publish = on_publish                          #assign function to callback
+client1.connect(broker,port)                                 #establish connection
+                   #publish
+ser = serial.Serial('COM7',11520)
 serData = ser.readline().decode('utf-8')
-things = str.encode('b')
-ser.write(things)
-counter = 0
+#things = str.encode('b')
+#ser.write(things)
+#counter = 0
 
 while True:
     serData = ser.readline().decode('utf-8')
-    imux,imuy,imuz = serData.split(';')
-    print(imux,imuy,imuz)
-    #print(serData)
+    #imux,imuy,imuz = serData.split('\n')
+    #print(imux,imuy,imuz)
+    print(serData)
+    payload = int(serData)
+    ret= client1.publish("sensor/ServoAngle",payload)
 
